5401e5d381f12a8591196f59c4eeaaa37e814d45
JensonCollins <jenson.collins@hotmail.com>
2022-09-09 20:14:56 +0900

    unit test

--- vue/src/store/generated/JensonCollins/checkers/jensoncollins.checkers.checkers/module/rest.ts
+++ vue/src/store/generated/JensonCollins/checkers/jensoncollins.checkers.checkers/module/rest.ts
@@ -9,6 +9,10 @@
  * ---------------------------------------------------------------
  */
 
+export interface CheckersMsgCreateGameResponse {
+  idValue?: string;
+}
+
 export interface CheckersNextGame {
   creator?: string;
 
@@ -109,6 +113,13 @@ export interface V1Beta1PageRequest {
    * is set.
    */
   count_total?: boolean;
+
+  /**
+   * reverse is set to true if results are to be returned in the descending order.
+   *
+   * Since: cosmos-sdk 0.43
+   */
+  reverse?: boolean;
 }
 
 /**
@@ -370,6 +381,7 @@ export class Api<SecurityDataType extends unknown> extends HttpClient<SecurityDa
       "pagination.offset"?: string;
       "pagination.limit"?: string;
       "pagination.count_total"?: boolean;
+      "pagination.reverse"?: boolean;
     },
     params: RequestParams = {},
   ) =>
